761cf49872556abf5659e0edfcd821eb6f716565
Monicca C. Michael <v-mcmichael@microsoft.com>
2023-09-19 06:50:23 +0000

    test

--- SimpleAppWithEmptyTemplate1/Program.cs
+++ SimpleAppWithEmptyTemplate1/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 app.UseFileServer();
 //app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.MapRazorPages();
 
 app.Run();
